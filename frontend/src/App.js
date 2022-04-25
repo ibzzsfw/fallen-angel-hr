1bac5eae1c7dfac09f1c3f253040ce50d6966122
ibzzsfw <tap.pt33@gmail.com>
2022-04-25 15:43:13 +0700

    sass broken

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { Content } from 'carbon-components-react';
 import './App.scss';
-import './App.css';
 import UI from './components/UI/UI';
 import Profile from './pages/Profile/Profile';
 import DailyTime from './pages/DailyTime/DailyTime';
