7f69d8a82f3958c1759af4c24642b453981a4922
Banky3011 <bankbiby@gmail.com>
2022-05-04 14:15:13 +0700

    add controller

--- server/controllers/UserRole.js
+++ server/controllers/UserRole.js
@@ -1,5 +1,4 @@
 const UserRole = (req, res, connection) => {
-
     connection.query("SELECT * FROM UserRole", (err, result) => {
         if (err) {
             console.log(err);
@@ -10,4 +9,126 @@ const UserRole = (req, res, connection) => {
     });
 };
 
-export default UserRole;
+const ShowInfo = (req, res, connection) => {
+    let employeeID = (req.headers.employeeID || '')
+    connection.query("SELECT * FROM Information WHERE employeeID = ?", [employeeID], function(err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+            res.send(result);
+        }
+    });
+};
+
+/*
+const ShowInfo = (req, res, connection) => {
+    let employeeID = (req.heders.employeeID || '')
+    connection.query("SELECT * FROM Information WHERE employeeID = employeeID", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+            res.send(result);
+        }
+    });
+};
+*/
+const ShowEducation = (req, res, connection) => {
+    let employeeID = (req.headers.employeeID || '')
+    connection.query("SELECT * FROM Education WHERE employeeID = ?", [employeeID], function(err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+            res.send(result);
+        }
+    });
+};
+
+const ShowPromoHis = (res, req, connection) => {
+    let employeeID = (req.headers.employeeID || '')
+    connection.query("SELECT * FROM PromotionHistory WHERE employeeID = ?", [employeeID], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+            res.send(result);
+        }
+    });
+};
+
+const IncomeByMonth = (res, req, connection) => {
+    let salary = (req.headers.salary || '')
+    let OT = (req.headers.OT || '')
+    let other = (req.headers.other || '')
+    let employeeID = (req.headers.employeeID || '')
+    connection.query("SELECT SUM(? + ? + ?) FROM Income WHERE employeeID = ? GROUP BY month;", [salary, OT, other, employeeID], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+            res.send(result);
+        }
+    });
+};
+
+const ShowPosName = (res, req, connection) => {
+    let Position = (req.headers.Position || '')
+    let positionID = (req.headers.positionID || '')
+        //let positionName = (req.headers.positionName || '')
+        //let OverTime = (req.headers.OverTime || '')
+    connection.query("SELECT Position.positionName FROM Position, OverTime WHERE OverTime.positionID = ?.? AND OverTime.employeeID = ?;", [Position, positionID, employeeID], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+            res.send(result);
+        }
+    });
+};
+
+const LeaveCount = (res, req, connection) => {
+    let status = (req.headers.status)
+    let employeeID = (req.headers.employeeID)
+    if (status == 'approved') {
+        connection.query("SELECT COUNT(status) FROM LeaveApplication WHERE status = ? AND employeeID = ?;", [status, employeeID], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(result);
+                res.send(result);
+            }
+        });
+    } else if (status == 'waiting') {
+        connection.query("SELECT COUNT(status) FROM LeaveApplication WHERE status = ? AND employeeID = ?;", [status, employeeID], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(result);
+                res.send(result);
+            }
+        });
+    } else if (status == 'rejected') {
+        connection.query("SELECT COUNT(status) FROM LeaveApplication WHERE status = ? AND employeeID = ?;", [status, employeeID], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(result);
+                res.send(result);
+            }
+        });
+    }
+};
+
+
+
+export default {
+    UserRole,
+    ShowInfo,
+    ShowEducation,
+    ShowPromoHis,
+    IncomeByMonth,
+    ShowPosName,
+    LeaveCount,
+}
--- server/index.js
+++ server/index.js
@@ -4,6 +4,12 @@ import cors from "cors";
 
 // controllers import
 import UserRole from './controllers/userRole.js';
+import ShowInfo from './controllers/UserRole.js';
+import ShowEducation from './controllers/UserRole.js';
+import ShowPromoHis from './controllers/UserRole.js';
+import IncomeByMonth from './controllers/UserRole.js';
+import ShowPosName from './controllers/UserRole.js';
+import LeaveCount from './controllers/UserRole.js';
 
 const connection = mysql.createConnection({
     host: 'SG-fallen-angel-hr-6098-mysql-master.servers.mongodirector.com',
@@ -28,5 +34,23 @@ app.get("/getLeaveType", (req, res) => {
     });
 });
 // app.get("/getUserRole", (req, res) => UserRole.UserRole(req, res, connection));
+//app.get("/getShowInfo", (req, res) => UserRole.ShowInfo(req, res, connection))
+//app.get("/getShowEducation", (req, res) => UserRole.ShowEducation(req, res, connection))
+//app.get("/getShowPromoHis", (req, res) => UserRole.ShowPromoHis(req, res, connection))
+//app.get("/getIncomeByMonth", (req, res) => UserRole.IncomeByMonth(req, res, connection))
+//app.get("/getShowPosName", (req, res) => UserRole.ShowPosName(req, res, connection))
+//app.get("/getLeaveCount", (req, res) => UserRole.LeaveCount(req, res, connection))
+
+
+
+app.get("/", (res, req) => {
+    connection.query("", (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    })
+});
 
 app.listen(3306, () => console.log("server is running on port 3306"));
